ab3eb071c635a08af778ac053668eeca28c61411
Luis Quesada Torres <luis@elezeta.com>
2019-10-28 16:46:12 +0100

    Small fix

--- generate.sh
+++ generate.sh
@@ -4,3 +4,4 @@ alias pdf2htmlEX='docker run -ti --rm -v `pwd`:/pdf bwits/pdf2htmlex pdf2htmlEX'
 pdf2htmlEX --zoom 1.5 --process-outline 0 resume.pdf
 cat resume.html | sed s/'<head>'/'<head>\n<title>Luis Quesada Torres - Resume - www.luisquesada.com<\/title>'/ > tmp.html
 mv tmp.html resume.html
+rm -f resume.aux resume.log resume.out
