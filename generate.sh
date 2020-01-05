b8061e0c9ac4dd04e782926f534494f92e4c3b11
Luis Quesada Torres <luis@elezeta.com>
2020-01-05 01:25:26 +0100

    Disable ligatures because they break some of the output in some browsers

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 xelatex resume.tex
 alias pdf2htmlEX='docker run -ti --rm -v `pwd`:/pdf bwits/pdf2htmlex pdf2htmlEX'
-pdf2htmlEX --zoom 1.5 --hdpi 600 --vdpi 600 --process-outline 0 resume.pdf
+pdf2htmlEX --decompose-ligature 1 --zoom 1.5 --hdpi 600 --vdpi 600 --process-outline 0 resume.pdf
 cat resume.html | sed s/'<head>'/'<head>\n<title>Luis Quesada Torres - Resume - www.luisquesada.com<\/title>'/ > tmp.html
 mv tmp.html resume.html
 rm -f resume.aux resume.log resume.out
