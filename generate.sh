7d4ab17d42c4b36272dd6ed218958bf985b5ed1d
Luis Quesada Torres <luis@elezeta.com>
2020-05-02 22:28:14 +0200

    Update with site

--- generate.sh
+++ generate.sh
@@ -3,5 +3,6 @@ xelatex resume.tex
 alias pdf2htmlEX='docker run -ti --rm -v `pwd`:/pdf bwits/pdf2htmlex pdf2htmlEX'
 pdf2htmlEX --decompose-ligature 1 --zoom 1.5 --hdpi 600 --vdpi 600 --process-outline 0 resume.pdf
 cat resume.html | sed s/'<head>'/'<head>\n<title>Luis Quesada Torres - Resume<\/title>\n<link rel="shortcut icon" type="image\/png" href="\/favicon.png"\/>'/ > tmp.html
-mv tmp.html resume.html
+mv -f tmp.html resume.html
 rm -f resume.aux resume.log resume.out
+mv -f resume.html index.html
